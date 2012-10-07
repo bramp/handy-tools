a4a7dd76244baadc169da9eec1305f96392cc724
Andrew Brampton <me@bramp.net>
2012-10-07 13:21:33 -0400

    Added a new SVG heatmap generating app, and other minor changes.

--- heatmap.py
+++ heatmap.py
@@ -9,6 +9,11 @@ import sys
 from collections import defaultdict
 from itertools import *
 from math import *
+import dateutil.parser
+import dateutil
+import svgwrite
+from brewer2mpl import sequential
+from bisect import bisect_left
 
 def hist_fd(data):
 	"""TODO Use Freedman-Diaconis' choice"""
@@ -29,7 +34,7 @@ def hist_ss(data):
 	for N in xrange(2,15): # Try values between 2 and 30
 		width = diff / float(N)
 
-		bins = bin_dict(data, N)
+		bins = hist_dict(data, N)
 		(mean, var) = bin_mean_var(bins)
 
 		C = (2.0 * mean - var) / (width * width)
@@ -52,20 +57,36 @@ def bin_mean_var(bins):
 	N = len(bins)
 	return (sum / N, (sum2 - (sum*sum) / N) / N)
 
-def bin_dict(data, N):
+def hist_dict(data, N = None, limits = None):
 	"""Places the data into N bins. We assume the data is a dict of X=>count pairs
 	   returns a dict of size N with (upper limit => count)
 	"""
 
-	keys = sorted(data.keys())
-	min  = keys[0]
-	max  = keys[-1]
-	diff = max - min
+	if len(data) == 0:
+		return data
+		#raise ValueError("data must have atleast one value")
 
-	limits = [min + i * diff / N for i in range(1, N + 1)]
+	keys = sorted(data.keys())
+
+	if limits is None:
+		if N is None:
+			raise ValueError("either N or limit must be set")
+
+		min_ = keys[0]
+		max_ = keys[-1]
+
+		diff = max_ - min_
+		limits = [min_ + i * diff / N for i in range(1, N + 1)]
+	elif N is None:
+		N = len(limits)
+		if N < 2:
+			raise ValueError("limits must have atleast 2 elements")
+	else:
+		raise ValueError("both N and limit can not be set")
 
 	bins = dict( zip(range(0, N), repeat(0, N)) )
 
+	# Assume keys is sorted
 	i = 0
 	for k in keys:
 		# Skip to the correct bin
@@ -73,7 +94,6 @@ def bin_dict(data, N):
 			i+=1
 
 		bins[ i ] += data[k]
-#		bins[ floor( (k - min) / diff * (N-1) )] += data[k]
 
 	return dict( zip ( limits, bins.itervalues() ) )
 
@@ -176,21 +196,124 @@ def summary_print(hist):
 		median=p50, p90=p90, p95=p95, p99=p99)
 
 
-def main(f, bins = 10, min = None, max = None):
-	raw = defaultdict(int)
+def main(f, bins = 10, min_ = None, max_ = None):
+	raw    = defaultdict(int)
+	dates  = defaultdict(int)
+	values = defaultdict(int)
+
 	for line in f:
-		try:
-			line = line.strip()
-			x = float(line)
-			raw[x] += 1
-		except ValueError:
-			print >> sys.stderr, "invalid value '{err}'".format(err=line)
+		line = line.strip()
+		if len(line) == 0:
 			continue
 
+		data = line.rsplit(None, 1)
+		if len(data) != 2:
+			print >> sys.stderr, "invalid line '{err}'".format(err=line)
+			continue
+
+		try:
+			date = dateutil.parser.parse(data[0])
+			#print date
+
+		except ValueError:
+			print >> sys.stderr, "invalid date '{err}'".format(err=data[0])
+			continue
+
+			#print
+		try:
+			x = float(data[1])
+		except ValueError:
+			print >> sys.stderr, "invalid value '{err}'".format(err=data[1])
+			continue
+
+		values [ x ] += 1
+		dates  [ date ] += 1
+		raw    [ (date, x) ] += 1
+
+	dates_N = 20
+	values_N = 10
+
 	if len(raw) > 0:
-		hist = hist_ss(raw)
-		hist_print(hist)
-		summary_print(raw)
+		# Now bin the dates
+		#dates_hist  = hist_dict(dates, N = dates_N)
+		#values_hist = hist_dict(values, N = values_N)
+		dates_hist  = hist_ss(dates)
+		values_hist = hist_ss(values)
+
+		limits = sorted(values_hist.keys())
+
+		first_filter = lambda x: x[0][0] <= max_
+		other_filter = lambda x: x[0][0] > min_ and x[0][0] <= max_
+		current_filter = first_filter
+
+		hist_2d = dict()
+		for max_, dates_count in sorted(dates_hist.items()):
+
+			h = defaultdict(int)
+			for k,v in filter(current_filter, raw.iteritems()):
+				h[ k[1] ] += v
+			current_filter = other_filter
+
+#			if first:
+#				h = defaultdict()
+#				#h = dict((k[1],v) for k,v in filter(lambda k, v: k[0] <= max_, raw.iteritems())# if k[0] <= max_)
+#				for k,v in raw.iteritems():
+#					if k[0] <= max_: h[k[1]] += v	
+#				first = False
+#			else:
+#				#h = dict((k[1],v) for k,v in raw.iteritems() if k[0] > min_ and k[0] <= max_)
+#				h = defaultdict()
+#				for k,v in raw.iteritems():
+#					if k[0] > min_ and k[0] <= max_: h[k[1]] += v	
+
+			new_dates_count = reduce(lambda x,y: x + y, h.itervalues())
+			assert new_dates_count == dates_count, "filtered list contains wrong number of values %d vs %d" % (len(h), dates_count)
+
+			hist_2d[ max_ ] = hist_dict(h, limits = limits)
+			min_ = max_
+
+
+		svg = svgwrite.Drawing('test.svg', profile='full')
+		#svg.add(dwg.line((0, 0), (100, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
+		#svg.add(dwg.text('Test', insert=(0, 0.2), fill='red'))
+
+		box_width  = 24
+		box_height = 14
+
+		colors = sequential.Blues[9].hex_colors
+
+		dx = 100
+		x = 0
+		y = 0
+
+		# Draw label
+		label_style = "text-anchor: end; dominant-baseline: central; font-size: 11px; font-family: sans-serif;"
+
+		for k in values_hist.keys():
+			y += box_height
+			svg.add( svg.text( k, insert=(dx - 5, y - box_height / 2), style=label_style ) )
+
+		# Draw main grid
+		x = 0
+		for date, counts in sorted(hist_2d.items()):
+			y = 0
+			for k, v in sorted(counts.items(), reverse=True):
+				#c = bisect_left(limits, k)
+				c = (v * 9) / 1000
+				r = svg.rect((x + dx,y), (box_width, box_height), fill = colors[c], stroke = "white")
+				r.set_desc( v )
+				svg.add( r )
+
+				y += box_height
+			x += box_width
+
+			print date, counts
+
+		svg.save()
+
+#		hist = hist_ss(raw)
+#		hist_print(hist)
+#		summary_print(raw)
 
 #f = open(filename, 'r')
 #for line in f:
